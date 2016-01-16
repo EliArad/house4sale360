fb601dcf3d0096eb0dbe67acd726edb1e87be20e
Eli Arad <easp13@gmail.com>
2016-01-16 21:46:45 +0200

    add create table script for nodejs

---
+++ createtables.js
@@ -0,0 +1,231 @@
+var mysqlserver = require('./server/common/mysql').server;
+var sqlserver = new mysqlserver();
+
+
+sqlserver.get(function (err, con) {
+    if (!err) {
+      var sql = "\
+      CREATE TABLE `renthousedetails` (\
+       `id` int(11) NOT NULL AUTO_INCREMENT,\
+       `propertyType` varchar(45) DEFAULT NULL,\
+       `city` varchar(100) DEFAULT NULL,\
+       `area` varchar(100) DEFAULT NULL,\
+       `neighborhood` varchar(100) DEFAULT NULL,\
+       `street` varchar(100) DEFAULT NULL,\
+       `floor` int(11) DEFAULT NULL,\
+       `fromfloor` int(11) DEFAULT '0',\
+       `squresize` int(11) DEFAULT NULL,\
+       `balcony` int(11) DEFAULT '0',\
+       `aircond` varchar(100) DEFAULT NULL,\
+       `parking` int(11) DEFAULT '0',\
+       `handycupt` int(11) DEFAULT '0',\
+       `mamad` bit(1) DEFAULT b'0',\
+       `warehouse` int(11) DEFAULT '0',\
+       `elevator` int(11) DEFAULT '0',\
+       `soragim` int(11) DEFAULT '0',\
+       `price` int(11) DEFAULT '0',\
+       `dateenter` date DEFAULT NULL,\
+       `immidiate` int(11) DEFAULT NULL,\
+       `callonsaturday` int(11) DEFAULT NULL,\
+       `freedetails` varchar(1000) DEFAULT NULL,\
+       `userid` int(11) DEFAULT NULL,\
+       `numberofrooms` int(11) DEFAULT NULL,\
+       `yearsofproperty` int(11) DEFAULT NULL,\
+       `parkingtype2` varchar(45) DEFAULT NULL,\
+       `homephone` int(11) DEFAULT NULL,\
+       `officephone` int(11) DEFAULT NULL,\
+       `secondarycellphone` int(11) DEFAULT NULL,\
+       `primarycellphone` int(11) DEFAULT NULL,\
+       `napa` varchar(145) DEFAULT NULL,\
+       `code` int(11) DEFAULT NULL,\
+       `housenumber` int(11) DEFAULT NULL,\
+       `parkingType` varchar(145) DEFAULT NULL,\
+       `email` varchar(245) DEFAULT NULL,\
+       `contactPerson` varchar(100) DEFAULT NULL,\
+       `renovated` varchar(75) DEFAULT NULL,\
+       `renovatedexplnation` varchar(245) DEFAULT NULL,\
+       `suspend` tinyint(4) DEFAULT NULL,\
+       PRIMARY KEY (`id`),\
+       KEY `area` (`area`),\
+       KEY `dateenter` (`dateenter`)\
+     ) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8";
+
+
+        var query = con.query(sql, function (err, rows) {
+            sqlserver.release(con);
+            if (err) {
+
+
+              console.log('error');
+            } else {
+              console.log('ok');
+            }
+    });
+  }
+})
+
+
+
+
+
+
+sqlserver.get(function (err, con) {
+    if (!err) {
+      var sql = "\
+      CREATE TABLE `renthouseblobs` (\
+        `id` int(11) NOT NULL AUTO_INCREMENT,\
+        `tableid` int(11) DEFAULT NULL,\
+        `filename` varchar(250) NOT NULL,\
+        `is360image` int(11) DEFAULT NULL,\
+        `is360video` int(11) DEFAULT NULL,\
+        `isvideo` int(11) DEFAULT NULL,\
+        PRIMARY KEY (`id`),\
+        UNIQUE KEY `id_UNIQUE` (`id`)\
+      ) ENGINE=InnoDB AUTO_INCREMENT=117 DEFAULT CHARSET=utf8;";
+
+
+        var query = con.query(sql, function (err, rows) {
+            sqlserver.release(con);
+            if (err) {
+
+
+              console.log('error');
+            } else {
+              console.log('ok');
+            }
+    });
+  }
+})
+
+
+
+
+sqlserver.get(function (err, con) {
+    if (!err) {
+      var sql = "\
+      CREATE TABLE `salehouseblobs` (\
+        `id` int(11) NOT NULL AUTO_INCREMENT,\
+        `tableid` int(11) DEFAULT NULL,\
+        `filename` varchar(250) NOT NULL,\
+        `is360image` int(11) DEFAULT NULL,\
+        `is360video` int(11) DEFAULT NULL,\
+        `isvideo` int(11) DEFAULT NULL,\
+        PRIMARY KEY (`id`),\
+        UNIQUE KEY `id_UNIQUE` (`id`)\
+      ) ENGINE=InnoDB AUTO_INCREMENT=117 DEFAULT CHARSET=utf8;";
+
+
+        var query = con.query(sql, function (err, rows) {
+            sqlserver.release(con);
+            if (err) {
+
+
+              console.log('error');
+            } else {
+              console.log('ok');
+            }
+    });
+  }
+})
+
+
+
+
+
+
+sqlserver.get(function (err, con) {
+    if (!err) {
+      var sql = "\
+      CREATE TABLE `sellhousedetails` (\
+        `id` int(11) NOT NULL AUTO_INCREMENT,\
+        `propertyType` varchar(45) DEFAULT NULL,\
+        `city` varchar(100) DEFAULT NULL,\
+        `area` varchar(100) DEFAULT NULL,\
+        `neighborhood` varchar(100) DEFAULT NULL,\
+        `street` varchar(100) DEFAULT NULL,\
+        `floor` int(11) DEFAULT NULL,\
+        `fromfloor` int(11) DEFAULT '0',\
+        `squresize` int(11) DEFAULT NULL,\
+        `balcony` int(11) DEFAULT '0',\
+        `aircond` varchar(100) DEFAULT NULL,\
+        `parking` int(11) DEFAULT '0',\
+        `handycupt` int(11) DEFAULT '0',\
+        `mamad` bit(1) DEFAULT b'0',\
+        `warehouse` int(11) DEFAULT '0',\
+        `elevator` int(11) DEFAULT '0',\
+        `soragim` int(11) DEFAULT '0',\
+        `price` int(11) DEFAULT '0',\
+        `dateenter` date DEFAULT NULL,\
+        `immidiate` int(11) DEFAULT NULL,\
+        `callonsaturday` int(11) DEFAULT NULL,\
+        `freedetails` varchar(1000) DEFAULT NULL,\
+        `userid` int(11) DEFAULT NULL,\
+        `numberofrooms` int(11) DEFAULT NULL,\
+        `yearsofproperty` int(11) DEFAULT NULL,\
+        `parkingtype2` varchar(45) DEFAULT NULL,\
+        `homephone` int(11) DEFAULT NULL,\
+        `officephone` int(11) DEFAULT NULL,\
+        `secondarycellphone` int(11) DEFAULT NULL,\
+        `primarycellphone` int(11) DEFAULT NULL,\
+        `napa` varchar(145) DEFAULT NULL,\
+        `code` int(11) DEFAULT NULL,\
+        `housenumber` int(11) DEFAULT NULL,\
+        `parkingType` varchar(145) DEFAULT NULL,\
+        `email` varchar(245) DEFAULT NULL,\
+        `contactPerson` varchar(100) DEFAULT NULL,\
+        `renovated` varchar(75) DEFAULT NULL,\
+        `renovatedexplnation` varchar(245) DEFAULT NULL,\
+        `suspend` tinyint(4) DEFAULT NULL,\
+        PRIMARY KEY (`id`),\
+        KEY `area` (`area`),\
+        KEY `dateenter` (`dateenter`)\
+      ) ENGINE=InnoDB AUTO_INCREMENT=124 DEFAULT CHARSET=utf8;";
+
+
+        var query = con.query(sql, function (err, rows) {
+            sqlserver.release(con);
+            if (err) {
+
+
+              console.log('error');
+            } else {
+              console.log('ok');
+            }
+    });
+  }
+})
+
+
+
+
+
+
+
+sqlserver.get(function (err, con) {
+    if (!err) {
+      var sql = "\
+      CREATE TABLE `users` (\
+        `id` int(11) NOT NULL AUTO_INCREMENT,\
+        `email` varchar(45) CHARACTER SET latin1 DEFAULT NULL,\
+        `agent` varchar(45) NOT NULL DEFAULT '0',\
+        `password` varchar(400) CHARACTER SET latin1 DEFAULT NULL,\
+        `userguid` varchar(528) CHARACTER SET latin1 DEFAULT NULL,\
+        `host` varchar(145) CHARACTER SET latin1 DEFAULT NULL,\
+        PRIMARY KEY (`id`),\
+        UNIQUE KEY `email_UNIQUE` (`email`),\
+        KEY `email` (`email`)\
+      ) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8";
+
+
+        var query = con.query(sql, function (err, rows) {
+            sqlserver.release(con);
+            if (err) {
+
+
+              console.log('error');
+            } else {
+              console.log('ok');
+            }
+    });
+  }
+})
