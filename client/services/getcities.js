0f8988963a083ce4a1d2a34093614b8bfd07e251
Eli Arad <easp13@gmail.com>
2016-01-16 22:49:49 +0200

    fix the cities count

--- client/services/getcities.js
+++ client/services/getcities.js
@@ -1,9 +1,16 @@
 app.factory("citiesservice", function($http, $q,myConfig)
 {
     var citiesData = [];
+    var inaction  = false;
 
     function getcities(callback)
     {
+        if (inaction == true)
+        {
+          callback(null, citiesData);
+          return;
+        }
+          inaction = true;
         if (citiesData.length == 0) {
             //console.log('loading cities');
             var url = myConfig.url + '/api/getcities';
@@ -25,26 +32,31 @@ app.factory("citiesservice", function($http, $q,myConfig)
                     }
                     chunk = Math.min(50, size);
                     size-=chunk;
-                    //console.log('size:' + size);
+                    console.log('size:' + size);
                     url = myConfig.url + '/api/getcitieschunk';
                     index1 = index;
                     index+=chunk;
                     $http.post(url, {size: size , index:index1, chunk:chunk}).then(function(result){
+                      console.log('adding: ' + result.data.a.length);
                         for (var j = 0 ; j < result.data.a.length; j++) {
                             citiesData.push(result.data.a[j]);
                         }
                         if (result.data.size == 0)
                         {
-
                             //console.log('finished, we have the cities');
                             //console.log(citiesData);
+                            console.log('size of cities is: ' + citiesData.length);
+                            inaction = false;
+                            citiesData.sort();
                             callback(null, citiesData);
+
                         }
                     }).catch (function(result){
                         citiesData = [];
                         //console.log('error get cities');
                         errorHappend = true;
                         callback('error', result);
+                            inaction = false;
                     })
                 }
             }).catch(function(result){
