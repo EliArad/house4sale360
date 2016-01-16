82427a1ef487b58605102152250fce62b4009688
Eli Arad <elia@RF-Dynamics.local>
2016-01-16 12:34:26 +0200

    update the create sql

--- server/controller/createtables.sql
+++ server/controller/createtables.sql
@@ -13,49 +13,51 @@ CREATE TABLE `renthouseblobs` (
 
 
 
-CREATE TABLE `renthousedetails` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `propertyType` varchar(45) DEFAULT NULL,
-  `city` varchar(100) DEFAULT NULL,
-  `area` varchar(100) DEFAULT NULL,
-  `neighborhood` varchar(100) DEFAULT NULL,
-  `street` varchar(100) DEFAULT NULL,
-  `floor` int(11) DEFAULT NULL,
-  `fromfloor` int(11) DEFAULT '0',
-  `squresize` int(11) DEFAULT NULL,
-  `balcony` int(11) DEFAULT '0',
-  `aircond` varchar(100) DEFAULT NULL,
-  `parking` int(11) DEFAULT '0',
-  `handycupt` int(11) DEFAULT '0',
-  `mamad` bit(1) DEFAULT b'0',
-  `warehouse` int(11) DEFAULT '0',
-  `elevator` int(11) DEFAULT '0',
-  `soragim` int(11) DEFAULT '0',
-  `price` int(11) DEFAULT '0',
-  `dateenter` date DEFAULT NULL,
-  `immidiate` int(11) DEFAULT NULL,
-  `callonsaturday` int(11) DEFAULT NULL,
-  `freedetails` varchar(1000) DEFAULT NULL,
-  `userid` int(11) DEFAULT NULL,
-  `numberofrooms` int(11) DEFAULT NULL,
-  `yearsofproperty` int(11) DEFAULT NULL,
-  `parkingtype2` varchar(45) DEFAULT NULL,
-  `homephone` int(11) DEFAULT NULL,
-  `officephone` int(11) DEFAULT NULL,
-  `secondarycellphone` int(11) DEFAULT NULL,
-  `primarycellphone` int(11) DEFAULT NULL,
-  `napa` varchar(145) DEFAULT NULL,
-  `code` int(11) DEFAULT NULL,
-  `housenumber` int(11) DEFAULT NULL,
-  `parkingType` varchar(145) DEFAULT NULL,
-  `email` varchar(245) DEFAULT NULL,
-  `contactPerson` varchar(100) DEFAULT NULL,
-  `renovated` varchar(75) DEFAULT NULL,
-  `renovatedexplnation` varchar(245) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `area` (`area`),
-  KEY `dateenter` (`dateenter`)
-) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8;
+  CREATE TABLE `renthousedetails` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `propertyType` varchar(45) DEFAULT NULL,
+   `city` varchar(100) DEFAULT NULL,
+   `area` varchar(100) DEFAULT NULL,
+   `neighborhood` varchar(100) DEFAULT NULL,
+   `street` varchar(100) DEFAULT NULL,
+   `floor` int(11) DEFAULT NULL,
+   `fromfloor` int(11) DEFAULT '0',
+   `squresize` int(11) DEFAULT NULL,
+   `balcony` int(11) DEFAULT '0',
+   `aircond` varchar(100) DEFAULT NULL,
+   `parking` int(11) DEFAULT '0',
+   `handycupt` int(11) DEFAULT '0',
+   `mamad` bit(1) DEFAULT b'0',
+   `warehouse` int(11) DEFAULT '0',
+   `elevator` int(11) DEFAULT '0',
+   `soragim` int(11) DEFAULT '0',
+   `price` int(11) DEFAULT '0',
+   `dateenter` date DEFAULT NULL,
+   `immidiate` int(11) DEFAULT NULL,
+   `callonsaturday` int(11) DEFAULT NULL,
+   `freedetails` varchar(1000) DEFAULT NULL,
+   `userid` int(11) DEFAULT NULL,
+   `numberofrooms` int(11) DEFAULT NULL,
+   `yearsofproperty` int(11) DEFAULT NULL,
+   `parkingtype2` varchar(45) DEFAULT NULL,
+   `homephone` int(11) DEFAULT NULL,
+   `officephone` int(11) DEFAULT NULL,
+   `secondarycellphone` int(11) DEFAULT NULL,
+   `primarycellphone` int(11) DEFAULT NULL,
+   `napa` varchar(145) DEFAULT NULL,
+   `code` int(11) DEFAULT NULL,
+   `housenumber` int(11) DEFAULT NULL,
+   `parkingType` varchar(145) DEFAULT NULL,
+   `email` varchar(245) DEFAULT NULL,
+   `contactPerson` varchar(100) DEFAULT NULL,
+   `renovated` varchar(75) DEFAULT NULL,
+   `renovatedexplnation` varchar(245) DEFAULT NULL,
+   `suspend` tinyint(4) DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   KEY `area` (`area`),
+   KEY `dateenter` (`dateenter`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8;
+
 
 
 CREATE TABLE `salehouseblobs` (
@@ -110,23 +112,23 @@ CREATE TABLE `sellhousedetails` (
   `contactPerson` varchar(100) DEFAULT NULL,
   `renovated` varchar(75) DEFAULT NULL,
   `renovatedexplnation` varchar(245) DEFAULT NULL,
+  `suspend` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `area` (`area`),
   KEY `dateenter` (`dateenter`)
-) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8;
-
-
+) ENGINE=InnoDB AUTO_INCREMENT=124 DEFAULT CHARSET=utf8;
+SELECT * FROM yad2vr.sellhousedetails;
 
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
-  `agent` varchar(45) DEFAULT '0',
+  `agent` varchar(45) NOT NULL DEFAULT '0',
   `password` varchar(400) CHARACTER SET latin1 DEFAULT NULL,
   `userguid` varchar(528) CHARACTER SET latin1 DEFAULT NULL,
   `host` varchar(145) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8;
-SELECT * FROM yad2vr.users;
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8;
+
