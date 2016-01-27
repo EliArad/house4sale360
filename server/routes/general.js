d38a1ae005c02251cb2efb578bbd6f069b2bfa60
Eli Arad <elia@RF-Dynamics.local>
2016-01-28 00:30:24 +0200

    add new file

---
+++ server/routes/general.js
@@ -0,0 +1,27 @@
+'use_strict';
+
+var express = require('express');
+var jwtauth = require('../common/jwtauth');
+
+module.exports = function (app,visitorController) {
+
+
+    function init() {
+
+
+        var router = express.Router();
+        router.post('/setvisitor', visitorController.setvisitor);
+        router.get('/getallvisitors', visitorController.getallvisitors);
+        router.get('/isverified',  visitorController.isverified);
+        router.post('/contactus', visitorController.contactus);
+
+
+        app.use('/api/general', router);
+
+    }
+
+    return {
+        init:init
+    }
+
+}
